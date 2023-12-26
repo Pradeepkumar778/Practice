51a1309e1fc4a1cbe24a652d6dd1da3726a41432
Pradeep Kumar Pathapalem <pradeeppathapalem@gmail.com>
2023-12-26 20:53:51 +0530

    java file

---
+++ Practice - project 1/AssistedProject/src/assisted/accessModifiers.java
@@ -0,0 +1,35 @@
+package assisted;
+
+class modifiers{
+	
+	public void Sample1()
+	{
+		System.out.println("This is public access modifier");
+	}
+	private void Sample2()
+	{
+		System.out.println("This is private access modifier");
+	}
+	protected void Sample3()
+	{
+		System.out.println("This is protected access modifier");
+	}
+	void sample4()
+	{
+		System.out.println("This is Default access modifier");
+	}
+}
+
+
+public class accessModifiers {
+
+	public static void main(String[] args) {
+		modifiers mod = new modifiers();
+		mod.Sample1();
+		//mod.sample2();
+		mod.Sample3();
+		mod.sample4();
+
+	}
+
+}
